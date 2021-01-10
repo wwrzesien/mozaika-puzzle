3f964c3b5658f3ed967050a3207331deacf90f84
Wojtek <wrzesien.ww@gmail.com>
2021-01-10 19:57:17 +0100

    Display puzzle with gaps.

--- mozaikaApp/app/Main.hs
+++ mozaikaApp/app/Main.hs
@@ -1,4 +1,5 @@
 import System.IO (readFile)
+
 -- wczytaj łamigłówkę z pliku o podanej nazwie
 readPuzzle :: String -> IO [String]
 readPuzzle filename = do
@@ -6,7 +7,30 @@ readPuzzle filename = do
     let puzzle = read contents :: [String] -- utwórz listę napisów (zob. klasa typów Read)
     return puzzle
 
+-- checkValue::String -> IO()
+-- checkValue row = if elem '5' row then putStrLn("tak")
+--     else putStrLn("nie")
+
+--- oddziel komórki lamigłówki znakiem
+insertSignInRow::String -> Char -> String
+insertSignInRow [] a = []
+insertSignInRow [x] a = [x]
+insertSignInRow (x:xs) a = x : a : insertSignInRow xs a
+
+-- wyświetl lamigłówkę
+printPuzzle::[String] -> IO()
+printPuzzle [] = return ()
+printPuzzle (x:xs) = do
+    let y = insertSignInRow x ' '
+    putStrLn y
+    printPuzzle xs
+
 main = do 
     puzzle <- readPuzzle "puzzles/puzzle01.txt"
-    putStrLn (show $ length $ puzzle) -- wyświetl liczbę wierszy łamigłówki
-    putStrLn (show $ puzzle) -- wyświetl łamigłówkę
+    putStrLn ("Łamigłówka:")
+    printPuzzle puzzle -- wyświetl łamigłówkę
+
+    -- checkValue (puzzle !! 0)
+
+
+
